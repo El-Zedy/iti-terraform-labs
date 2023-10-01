f7160cfdbc3b36cca9f71e4d9b53c7f70518b6dc
Muhammad ElZedy <30501463+El-Zedy@users.noreply.github.com>
2023-10-01 15:26:52 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,6 @@
 provider "aws" {
-  shared_config_files      = ["/aws/conf"]
-  shared_credentials_files = ["/aws/creds"]
+  shared_config_files      = ["/root/.aws/config"]
+  shared_credentials_files = ["/root/.aws/credentials"]
   region  = "us-east-1"
   profile                  = "default"
-
 }
