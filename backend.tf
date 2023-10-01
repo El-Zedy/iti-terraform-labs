fdecf6ee89f8aec40e0c1f6723a6a425228eeba1
Muhammad ElZedy <30501463+El-Zedy@users.noreply.github.com>
2023-10-01 15:10:02 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,5 +1,7 @@
 terraform {
     backend "s3" {
+      shared_config_files      = ["/aws/conf"]
+      shared_credentials_files = ["/aws/creds"]
       bucket  = "terraform-backend-bucket-iti-test"
       dynamodb_table = "terraform-state-lock-iti-test"
       key     = "terraform.tfstate"
